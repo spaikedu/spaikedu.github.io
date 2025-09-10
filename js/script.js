2171b1c81380e4ffd7d4f23917db63fd603cdbd0
Tu Nombre <emartinez.aquihaymarketing@gmail.com>
2025-09-10 16:00:08 +0200

    mas cambios

--- js/script.js
+++ js/script.js
@@ -7,6 +7,33 @@ document.addEventListener('DOMContentLoaded', () => {
         offset: 100, // offset (in px) from the original trigger point
     });
 
+    // Slick Slider for Projects
+    $('.projects-grid').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
     // Custom Cursor Logic
     const cursor = document.querySelector('.cursor');
     const follower = document.querySelector('.cursor-follower');
@@ -32,4 +59,10 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    console.log(
+        "%c¡Hola, developer curioso! \n\nSi estás leyendo esto, es porque te gusta mirar bajo el capó. 😉\n\n¿Interesado en colaborar o simplemente quieres charlar sobre código?\n¡No dudes en contactarme! \n\nLinkedIn: https://www.linkedin.com/in/eduardo-m-05a523124/",
+        "font-family:monospace;color:#007bff;font-size:16px;font-weight:bold;"
+      );
+
 });
+
